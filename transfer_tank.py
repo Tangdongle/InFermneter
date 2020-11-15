71eb4dd8159f6d69147fabd12b41063db797e790
ryan <ryanc@lastyard.com>
2020-11-15 12:26:24 +0000

    Added IO cleanup and changed default pump states to match relay

--- transfer_tank.py
+++ transfer_tank.py
@@ -1,5 +1,9 @@
 ###
-### Transfer tank sensors are active (1) when down
+### Transfer tank sensors are active (1) when down (liquid lifts them up)
+###
+### Pump relay defaults to NC (Not connected) side
+### so booleans for pump output are inverted
+###
 ### Pump needs to be activated when both sensors are 0
 ### Pump stays active until bottom sensor is back on
 ###
@@ -19,24 +23,25 @@ IO.setup(TANK_BOTTOM_GPIO_IN, IO.IN)
 IO.setup(TANK_TOP_GPIO_IN, IO.IN)
 IO.setup(PUMP_GPIO_OUT, IO.OUT)
 
-IO.output(PUMP_GPIO_OUT, IO.LOW)
+IO.output(PUMP_GPIO_OUT, IO.HIGH)
 
-while True:
-  top_val = IO.input(TANK_TOP_GPIO_IN)
-  bot_val = IO.input(TANK_BOTTOM_GPIO_IN)
+try:
+  while True:
+    top_val = IO.input(TANK_TOP_GPIO_IN)
+    bot_val = IO.input(TANK_BOTTOM_GPIO_IN)
 
-  # If both the top and bottom sensors are off (0)
-  # we need to start the draining cycle
-  if (top_val == False and bot_val == False and not DRAINING):
-    print("Draining enabled...")
-    DRAINING = True
-    IO.output(PUMP_GPIO_OUT, IO.HIGH)
+    # If both the top and bottom sensors are off (0)
+    # we need to start the draining cycle
+    if (top_val == False and bot_val == False and not DRAINING):
+      DRAINING = True
+      IO.output(PUMP_GPIO_OUT, IO.LOW)
 
-  # If we have been draining and the bottom sensor turns on
-  # we have finished draining and can disable the pump
-  if (DRAINING and bot_val == True):
-    print("Draining complete.")
-    DRAINING = False
-    IO.output(PUMP_GPIO_OUT, IO.LOW)
+    # If we have been draining and the bottom sensor turns on
+    # we have finished draining and can disable the pump
+    if (DRAINING and bot_val == True):
+      DRAINING = False
+      IO.output(PUMP_GPIO_OUT, IO.HIGH)
 
-  time.sleep(0.5)
+    time.sleep(0.5)
+finally:
+  IO.cleanup()
