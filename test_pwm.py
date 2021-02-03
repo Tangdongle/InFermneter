ccde769098a96947a37b47798533a584b3f08c6f
Ryan Cotter <ryanc@lastyard.com>
2021-02-03 22:27:12 +0800

    Initial test prototype

---
+++ test_pwm.py
@@ -0,0 +1,17 @@
+import wiringpi
+
+OUTPUT = 1
+
+wiringpi.wiringPiSetup()
+channels = [17, 18, 19, 20, 21, 22, 23, 24, 25]
+for channel in channels:
+    wiringpi.pinMode(channel, OUTPUT)
+    wiringpi.softPwmCreate(channel, 0, 100)
+
+for time in range(0, 4):
+    for channel in channels:
+        for brightness in range(0, 100):
+            wiringpi.softPwmWrite(channel, 100)
+            wiringpi.delay(10)
+            wiringpi.softPwmWrite(channel, 0)
+        wiringpi.delay(10)
