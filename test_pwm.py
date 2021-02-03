96c9df0e29df09e5115f8802159a8c29a738f184
Ryan Cotter <ryanc@lastyard.com>
2021-02-03 23:29:52 +0800

    updated test file

--- test_pwm.py
+++ test_pwm.py
@@ -3,14 +3,11 @@ import wiringpi
 OUTPUT = 1
 
 wiringpi.wiringPiSetup()
-channels = [17, 18, 19, 20, 21, 22, 23, 24, 25]
-for channel in channels:
-    wiringpi.pinMode(channel, OUTPUT)
-    wiringpi.softPwmCreate(channel, 0, 100)
+channel = 17
+wiringpi.pinMode(channel, OUTPUT)
+wiringpi.softPwmCreate(channel, 0, 100)
 
-for time in range(0, 4):
-    for channel in channels:
-        wiringpi.softPwmWrite(channel, 100)
-    wiringpi.delay(10)
-    for channel in channels:
-        wiringpi.softPwmWrite(channel, 0)
+wiringpi.softPwmWrite(channel, 100)
+print("Starting loop")
+while True:
+    pass
