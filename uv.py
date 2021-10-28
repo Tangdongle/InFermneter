c5441fea3b2c3f4cf0a317e68ce8646d757d8bee
Ryan Cotter <ryanc@lastyard.com>
2021-10-28 18:30:08 +0800

    air and uv both run at the same time

--- uv.py
+++ uv.py
@@ -8,6 +8,9 @@ import RPi.GPIO as IO
 from datetime import datetime, timedelta, timezone
 import configparser
 import argparse
+import time
+
+from utilities import get_last_timestamp, update_timestamp_file
 
 parser = argparse.ArgumentParser(description="UV Manager")
 parser.add_argument(
@@ -24,39 +27,50 @@ CONFIGFILE = args.config_fname
 
 config = configparser.ConfigParser()
 config.read(CONFIGFILE)
+
+AIR_PUMP_GPIO_OUT = int(config["AIRPUMP"]["PUMP_GPIO_OUT"])
+if AIR_PUMP_GPIO_OUT == -1:
+    print("No GPIO set")
+    quit(1)
+
+# File to write timestamp of last run to
+AIR_TS_FILE = config["AIRPUMP"]["TS_FILE"]
+AIR_FREQUENCY = float(config["AIRPUMP"]["FREQUENCY"])
+CYCLE_TIME = int(config["AIRPUMP"]["CYCLE_TIME"])
+TS_STR_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
+# Strip the last characters from isoformat string to get rid of the pesky timezone
+TS_STR_REPLACE_TZ_OFFSET = -6
+
 PUMP_GPIO_OUT = int(config["UV"]["PUMP_GPIO_OUT"])
 if PUMP_GPIO_OUT == -1:
     print("No GPIO set")
     quit(1)
 
 # File to write timestamp of last run to
-TS_FILE = config["UV"]["TS_FILE"]
-TS_STR_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
+UV_TS_FILE = config["UV"]["TS_FILE"]
 FREQUENCY = int(config["UV"]["FREQUENCY"])
-# Strip the last characters from isoformat string to get rid of the pesky timezone
-TS_STR_REPLACE_TZ_OFFSET = -6
 
 IO.setmode(IO.BCM)
 IO.setup(PUMP_GPIO_OUT, IO.OUT, initial=IO.HIGH)
+IO.setup(AIR_PUMP_GPIO_OUT, IO.OUT, initial=IO.HIGH)
 
-NOFILE = False
-try:
-    with open(TS_FILE, "r") as ts:
-        timestamp = ts.read()[:TS_STR_REPLACE_TZ_OFFSET]
-        if timestamp and datetime.strptime(timestamp, TS_STR_FORMAT) >= datetime.now(timezone.utc) - timedelta(days=FREQUENCY):
-            print("Time's not up, keep waiting")
-            IO.output(PUMP_GPIO_OUT, IO.HIGH)  # make sure the UV is off
-        else:
-            # Switch the UV on and let cron do it's job of checking every X minutes
-            IO.output(PUMP_GPIO_OUT, IO.LOW)
-except FileNotFoundError:
-    NOFILE = True
+SECONDS_TO_WAIT_FOR_FOAM = 60 * 60 # One hour
+SECONDS_UV_ON = 20
 
-# If we have no timestamp, run anyway
-if NOFILE:
+
+def cycle_air_pump(seconds):
+    print(f"Turning air pump on for {seconds} seconds")
+    IO.output(AIR_PUMP_GPIO_OUT, IO.LOW)
+    time.sleep(seconds)
+    print("Turning air pump off")
+    IO.output(AIR_PUMP_GPIO_OUT, IO.HIGH)
+
+
+if __name__ == '__main__':
+    cycle_air_pump(CYCLE_TIME)
+    time.sleep(SECONDS_TO_WAIT_FOR_FOAM)
     IO.output(PUMP_GPIO_OUT, IO.LOW)
-# Write the timestamp
-with open(TS_FILE, "w") as ts:
-    timestamp = datetime.now(timezone.utc).isoformat()
-    ts.write(timestamp)
-# No cleanup
+    time.sleep(SECONDS_UV_ON)
+    IO.output(PUMP_GPIO_OUT, IO.HIGH)
+
+    IO.cleanup()
